638bb334ebf56b25fb53f01e8df8f5917193e74b
Aditya Bhattacharjee <adityab76@gmail.com>
2023-08-06 16:09:56 +0530

    Diet Page updated

--- src/Pages/DietPlan/BodyRecomp.jsx
+++ src/Pages/DietPlan/BodyRecomp.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import bulkdiet from '../../Images/bulkdiet.jpg';
 
-function BodyRecomp() {
+function Bulk() {
   return (
-    <div className='BodySec'>
-       
+    <div className="BulkSec">
+      <div className="bulkimg" >
+        <h1 style={{ color: "white" }}><img src={bulkdiet} alt="Bulk Diet" /></h1>
+      </div>
+      <div className="bulk-text" >
+        <h1 style={{ color: "white" }}>BULK : MAINTENANCE CALORIES + 500 CAL</h1>
+        <h3><u>FOOD ITEMS TO CONSUME DURING BULK</u></h3>
+        <br/>
+        <div className="food-category">
+          <h2>HEALTHY FATS:</h2>
+          <ul className="food-item">
+            <li>Whole Grains</li>
+            <li>Nuts</li>
+            <li>Pulses</li>
+            <li>Dairy Products</li>
+          </ul>
+        </div>
+        <br/> 
+        <div className="food-category">
+          <h2>PROTEIN:</h2>
+          <ul className="food-item">
+            <li>Chicken Breast / Thighs</li>
+            <li>Fishes: Basa, Salmon, Catfish, Prawns</li>
+            <li>Red Meat</li>
+          </ul>
+        </div> <br/>
+        <div className="food-category">
+          <h2>CARBOHYDRATES:</h2>
+          <ul className="food-item">
+            <li>Rice</li>
+            <li>Oats</li>
+            <li>Tortilla/Rotis/Pita</li>
+          </ul>
+        </div> <br/>
+        <div className="food-category">
+          <h2>MICRO-NUTRIENTS:</h2>
+          <ul className="food-item">
+            <li>Fruits</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default BodyRecomp
+export default Bulk;
--- src/Pages/DietPlan/Bulk.jsx
+++ src/Pages/DietPlan/Bulk.jsx
@@ -1,16 +1,55 @@
 import React from "react";
-
+import bulkdiet from '../../Images/bulkdiet.jpg';
 
 function Bulk() {
-  return <div className="BulkSec">
-    <div className="bulkimg" style={{border:"solid",borderColor:"white"}}>
-    <h1 style={{color:"white"}}>MAINTAINENCE CALORIES + 500 CALS</h1>
+  return (
+    <div className="BulkSec">
+      <div className="bulkimg" >
+        <h1 style={{ color: "white" }}><img src={bulkdiet} alt="Bulk Diet" /></h1>
+      </div>
+      <div className="bulk-text" >
+        <h1 style={{ color: "white" }}>BULK : MAINTENANCE CALORIES + 500 CAL</h1>
+        <h3><u>FOOD ITEMS TO CONSUME DURING BULK</u></h3>
+        <br/>
+        <div className="food-category">
+          
+          <ul className="food-item">
+          <h2>HEALTHY FATS:</h2>
+            <li>Whole Grains</li>
+            <li>Nuts</li>
+            <li>Pulses</li>
+            <li>Dairy Products</li>
+          </ul>
+        </div>
+        <br/> 
+        <div className="food-category">
+         
+          <ul className="food-item">
+          <h2>PROTEIN:</h2>
+            <li>Chicken Breast / Thighs</li>
+            <li>Fishes: Basa, Salmon, Catfish, Prawns</li>
+            <li>Red Meat</li>
+          </ul>
+        </div> <br/>
+        <div className="food-category">
+        
+          <ul className="food-item">
+          <h2>CARBOHYDRATES:</h2>
+            <li>Rice</li>
+            <li>Oats</li>
+            <li>Tortilla/Rotis/Pita</li>
+          </ul>
+        </div> <br/>
+        <div className="food-category">
+        
+          <ul className="food-item">
+          <h2>MICRO-NUTRIENTS:</h2>
+            <li>Fruits</li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <div className="bulk-text" style={{border:"solid",borderColor:"white"}}>
-        <h1 style={{color:"white"}}>MAINTAINENCE CALORIES + 500 CALS</h1>
-    </div>
-  
-  </div>;
+  );
 }
 
 export default Bulk;
